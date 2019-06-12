1568e3e2c5b8674425921dd9e5f2a748149352c5
hualong <magic.hualong@gmail.com>
2019-06-12 20:17:39 +0800

    调整方法位置

--- async/src/test/java/cc/stan/example/async/TestCompletableFuture.java
+++ async/src/test/java/cc/stan/example/async/TestCompletableFuture.java
@@ -6,35 +6,27 @@ import java.util.Random;
 import java.util.concurrent.*;
 
 public class TestCompletableFuture {
+
     private Random random = new Random();
 
-    @Test
-    public void test1() {
-
-        long start = System.nanoTime();
-
-        Future<Double> futurePrice = getPriceAsync("shop");
-
-        System.out.println("f1, " + (System.nanoTime() - start) / 1_000_000);
-
+    //sleep时间
+    private void delay() {
         try {
-            double price = futurePrice.get(2, TimeUnit.SECONDS);
-            System.out.printf("price %.2f%n", price);
+            Thread.sleep(1000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
         }
-        System.out.println("f2, " + (System.nanoTime() - start) / 1_000_000);
-        //TODO: Java in action 8 getPriceAsync
     }
 
-    @Test
-    public void test2() throws InterruptedException, ExecutionException, TimeoutException {
-        Future<Double> doubleFuture = getPriceAsyncHasException("shop");
-        doubleFuture.get(2, TimeUnit.SECONDS);
+    //根据product生成随机价格
+    private double calculatePrice(String product) {
+        delay();
+        return random.nextDouble() + product.charAt(1);
+    }
+
+    //同步的获取产品价格
+    private double getrice(String product) {
+        return calculatePrice(product);
     }
 
     //异步获取futurePrice
@@ -45,6 +37,20 @@ public class TestCompletableFuture {
             futurePrice.complete(price);
         }).start();
         return futurePrice;
+    }
+
+    @Test
+    public void testAsyncFunction() throws InterruptedException, ExecutionException, TimeoutException {
+        long start = System.nanoTime();
+
+        Future<Double> futurePrice = getPriceAsync("shop");
+
+        System.out.println("f1, " + (System.nanoTime() - start) / 1_000_000);
+
+        double price = futurePrice.get(2, TimeUnit.SECONDS);
+
+        System.out.printf("price %.2f%n", price);
+        System.out.println("f2, " + (System.nanoTime() - start) / 1_000_000);
     }
 
     //异步获取futurePrice，会抛出异常
@@ -62,24 +68,10 @@ public class TestCompletableFuture {
         return futurePrice;
     }
 
-    //同步的获取产品价格
-    private double getrice(String product) {
-        return calculatePrice(product);
-    }
-
-    //根据product生成随机价格
-    private double calculatePrice(String product) {
-        delay();
-        return random.nextDouble() + product.charAt(1);
-    }
-
-    //sleep时间
-    private void delay() {
-        try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void testAsyncFunctionHasException() throws InterruptedException, ExecutionException, TimeoutException {
+        Future<Double> doubleFuture = getPriceAsyncHasException("shop");
+        doubleFuture.get(2, TimeUnit.SECONDS);
     }
 
 }
